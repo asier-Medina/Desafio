3ad0f39a22123f10995bc3cfc077ec1a57d8a4ab
agonzalezbridge <asier.medina.ugalde@gmail.com>
2026-06-01 09:11:02 +0200

    quitar partes de proyecto anterior en auth

--- Backend/src/middlewares/auth.js
+++ Backend/src/middlewares/auth.js
@@ -25,16 +25,4 @@ export const isAdmin = (req, res, next) => {
   next()
 }
 
-export const isSameCompany = (req, res, next) => {
-  // Evita que un admin vea datos de otra empresa
-  if (req.user.role !== 'superadmin' &&
-      req.user.company_id !== parseInt(req.params.company_id)) {
-    return res.status(403).json({ error: 'Acceso denegado' })
-  }
-  next()
-}
-export const isAdminOrSuperAdmin = (req, res, next) => {
-  if (!['admin', 'superadmin'].includes(req.user.role))
-    return res.status(403).json({ error: 'Acceso denegado' })
-  next()
-}
+
