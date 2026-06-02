e056d3d76e3e66735e3c421db90d167d80067559
jefferson <jeffersonffers@hmail.com>
2026-06-02 18:57:36 +0200

    mejora del header y Mobile Nav

--- Frontend/src/shared/layout/Main.jsx
+++ Frontend/src/shared/layout/Main.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate, useLocation } from "react-router";
 import Header from "@shared/components/Header/Header.jsx";
+import MobileNav from "@shared/MobileNav/MobileNav";
 
 export default function MainLayout() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   return (
     <>
-      <Header />
+      <Header onNavigate={navigate} />
       <main>
         <Outlet />
       </main>
+      <MobileNav onNavigate={navigate} currentPath={pathname} />
     </>
   );
 }
